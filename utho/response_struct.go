3e05453eafbab39ae574d4896a8ca627c875346d
hmada15 <31375621+hmada15@users.noreply.github.com>
2024-05-03 17:38:48 +0300

    add Responses struct

--- utho/response_struct.go
+++ utho/response_struct.go
@@ -1,5 +1,10 @@
 package utho
 
+type BasicResponse struct {
+	Status  string `json:"status,omitempty"`
+	Message string `json:"message,omitempty"`
+}
+
 type DeleteResponse struct {
 	Status  string `json:"status,omitempty"`
 	Message string `json:"message,omitempty"`
