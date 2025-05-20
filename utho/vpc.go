ae4b075080fbe42c60695d6f0d5d15c8355e0c2b
hmada15 <31375621+hmada15@users.noreply.github.com>
2025-05-20 12:16:53 +0300

    refactor: add new VPC service methods and improve test coverage with faker

--- utho/vpc.go
+++ utho/vpc.go
@@ -2,6 +2,7 @@ package utho
 
 import (
 	"errors"
+	"fmt"
 )
 
 type VpcService service
@@ -75,12 +76,15 @@ func (s *VpcService) Read(vpcId string) (*Vpc, error) {
 	}
 
 	var vpc Vpc
+	found := false
 	for _, r := range vpcs.Vpc {
 		if r.ID == vpcId {
 			vpc = r
+			found = true
+			break
 		}
 	}
-	if len(vpc.ID) == 0 {
+	if !found {
 		return nil, errors.New("NotFound")
 	}
 
@@ -116,4 +120,267 @@ func (s *VpcService) Delete(vpcId string) (*DeleteResponse, error) {
 	}
 
 	return &delResponse, nil
+}
+
+type CreateSubnetParams struct {
+	Name         string `json:"name"`
+	VpcID        string `json:"vpcid"`
+	AssignPublic string `json:"assign_public"`
+	IsDefault    string `json:"is_default"`
+	Type         string `json:"type"`
+	Network      string `json:"network"`
+	Size         string `json:"size"`
+}
+
+func (s *VpcService) CreateSubnet(params CreateSubnetParams) (*CreateResponse, error) {
+	reqUrl := "vpc/subnet/create"
+	req, _ := s.client.NewRequest("POST", reqUrl, &params)
+
+	var subnet CreateResponse
+	_, err := s.client.Do(req, &subnet)
+	if err != nil {
+		return nil, err
+	}
+	if subnet.Status != "success" && subnet.Status != "" {
+		return nil, errors.New(subnet.Message)
+	}
+
+	return &subnet, nil
+}
+
+type CreateNatGatewayParams struct {
+	Subnet string `json:"subnet"`
+	Name   string `json:"name"`
+	UserID string `json:"userid"`
+}
+
+func (s *VpcService) CreateNatGateway(params CreateNatGatewayParams) (*CreateResponse, error) {
+	reqUrl := "vpc/natgateway"
+	req, _ := s.client.NewRequest("POST", reqUrl, &params)
+
+	var natGateway CreateResponse
+	_, err := s.client.Do(req, &natGateway)
+	if err != nil {
+		return nil, err
+	}
+	if natGateway.Status != "success" && natGateway.Status != "" {
+		return nil, errors.New(natGateway.Message)
+	}
+
+	return &natGateway, nil
+}
+
+type AllocateElasticIPParams struct {
+	Dcslug string `json:"dcslug"`
+}
+
+func (s *VpcService) AllocateElasticIP(params AllocateElasticIPParams) (*CreateResponse, error) {
+	reqUrl := "elasticip/allocate"
+	req, _ := s.client.NewRequest("POST", reqUrl, &params)
+
+	var elasticIP CreateResponse
+	_, err := s.client.Do(req, &elasticIP)
+	if err != nil {
+		return nil, err
+	}
+	if elasticIP.Status != "success" && elasticIP.Status != "" {
+		return nil, errors.New(elasticIP.Message)
+	}
+
+	return &elasticIP, nil
+}
+
+type DeallocateElasticIPParams struct {
+	IPAddress string
+	Subnet    string `json:"subnet"`
+	Name      string `json:"name"`
+	UserID    string `json:"userid"`
+}
+
+func (s *VpcService) DeallocateElasticIP(params DeallocateElasticIPParams) (*BasicResponse, error) {
+	reqUrl := "elasticip/" + params.IPAddress + "/deallocate"
+	req, _ := s.client.NewRequest("POST", reqUrl, &params)
+
+	var basicResponse BasicResponse
+	_, err := s.client.Do(req, &basicResponse)
+	if err != nil {
+		return nil, err
+	}
+	if basicResponse.Status != "success" && basicResponse.Status != "" {
+		return nil, errors.New(basicResponse.Message)
+	}
+
+	return &basicResponse, nil
+}
+
+type CreatePeeringConnectionParams struct {
+	Name           string `json:"name"`
+	Dcslug         string `json:"dcslug"`
+	RequesterVpcID string `json:"requester_vpc_id"`
+	AccepterVpcID  string `json:"accepter_vpc_id"`
+}
+
+func (s *VpcService) CreatePeeringConnection(params CreatePeeringConnectionParams) (*CreateResponse, error) {
+	reqUrl := "vpc/peering"
+	req, _ := s.client.NewRequest("POST", reqUrl, &params)
+
+	var peering CreateResponse
+	_, err := s.client.Do(req, &peering)
+	if err != nil {
+		return nil, err
+	}
+	if peering.Status != "success" && peering.Status != "" {
+		return nil, errors.New(peering.Message)
+	}
+
+	return &peering, nil
+}
+
+type UpdateRouteParams struct {
+	RouteID              string `json:"route_id"`
+	DestinationCidrBlock string `json:"destination_cidr_block"`
+	Target               string `json:"target"`
+	RouteType            string `json:"route_type"`
+}
+
+func (s *VpcService) UpdateRoute(params UpdateRouteParams) (*BasicResponse, error) {
+	reqUrl := "vpc/route"
+	req, _ := s.client.NewRequest("POST", reqUrl, &params)
+
+	var basicResponse BasicResponse
+	_, err := s.client.Do(req, &basicResponse)
+	if err != nil {
+		return nil, err
+	}
+	if basicResponse.Status != "success" && basicResponse.Status != "" {
+		return nil, errors.New(basicResponse.Message)
+	}
+
+	return &basicResponse, nil
+}
+
+type CreateInternetGatewayParams struct {
+	Subnet string `json:"subnet"`
+	Name   string `json:"name"`
+	UserID string `json:"userid"`
+}
+
+func (s *VpcService) CreateInternetGateway(params CreateInternetGatewayParams) (*CreateResponse, error) {
+	reqUrl := "vpc-internetgateway"
+	req, _ := s.client.NewRequest("POST", reqUrl, &params)
+
+	var internetGateway CreateResponse
+	_, err := s.client.Do(req, &internetGateway)
+	if err != nil {
+		return nil, err
+	}
+	if internetGateway.Status != "success" && internetGateway.Status != "" {
+		return nil, errors.New(internetGateway.Message)
+	}
+
+	return &internetGateway, nil
+}
+
+type AttachSubnetToInternetGatewayParams struct {
+	GatewayID string
+	Subnet    string `json:"subnet"`
+}
+
+func (s *VpcService) AttachSubnetToInternetGateway(params AttachSubnetToInternetGatewayParams) (*BasicResponse, error) {
+	reqUrl := fmt.Sprintf("vpc-internetgateway/%s/attach", params.GatewayID)
+	req, _ := s.client.NewRequest("POST", reqUrl, &params)
+
+	var basicResponse BasicResponse
+	_, err := s.client.Do(req, &basicResponse)
+	if err != nil {
+		return nil, err
+	}
+	if basicResponse.Status != "success" && basicResponse.Status != "" {
+		return nil, errors.New(basicResponse.Message)
+	}
+
+	return &basicResponse, nil
+}
+
+type DetachSubnetFromInternetGatewayParams struct {
+	GatewayID string
+	Subnet    string `json:"subnet"`
+}
+
+func (s *VpcService) DetachSubnetFromInternetGateway(params DetachSubnetFromInternetGatewayParams) (*BasicResponse, error) {
+	reqUrl := fmt.Sprintf("vpc-internetgateway/%s/dettach", params.GatewayID)
+	req, _ := s.client.NewRequest("POST", reqUrl, &params)
+
+	var basicResponse BasicResponse
+	_, err := s.client.Do(req, &basicResponse)
+	if err != nil {
+		return nil, err
+	}
+	if basicResponse.Status != "success" && basicResponse.Status != "" {
+		return nil, errors.New(basicResponse.Message)
+	}
+
+	return &basicResponse, nil
+}
+
+type CreateRouteTableParams struct {
+	VpcID string `json:"vpc_id"`
+}
+
+func (s *VpcService) CreateRouteTable(params CreateRouteTableParams) (*CreateResponse, error) {
+	reqUrl := "vpc-route-table"
+	req, _ := s.client.NewRequest("POST", reqUrl, &params)
+
+	var routeTable CreateResponse
+	_, err := s.client.Do(req, &routeTable)
+	if err != nil {
+		return nil, err
+	}
+	if routeTable.Status != "success" && routeTable.Status != "" {
+		return nil, errors.New(routeTable.Message)
+	}
+
+	return &routeTable, nil
+}
+
+type AssociateSubnetWithRouteTableParams struct {
+	SubnetID     string `json:"subnet_id"`
+	RouteTableID string `json:"route_table_id"`
+}
+
+func (s *VpcService) AssociateSubnetWithRouteTable(params AssociateSubnetWithRouteTableParams) (*BasicResponse, error) {
+	reqUrl := "vpc/subnet/associate"
+	req, _ := s.client.NewRequest("POST", reqUrl, &params)
+
+	var basicResponse BasicResponse
+	_, err := s.client.Do(req, &basicResponse)
+	if err != nil {
+		return nil, err
+	}
+	if basicResponse.Status != "success" && basicResponse.Status != "" {
+		return nil, errors.New(basicResponse.Message)
+	}
+
+	return &basicResponse, nil
+}
+
+type DissociateSubnetFromRouteTableParams struct {
+	SubnetID     string `json:"subnet_id"`
+	RouteTableID string `json:"route_table_id"`
+}
+
+func (s *VpcService) DissociateSubnetFromRouteTable(params DissociateSubnetFromRouteTableParams) (*BasicResponse, error) {
+	reqUrl := "vpc/subnet/dissociate"
+	req, _ := s.client.NewRequest("POST", reqUrl, &params)
+
+	var basicResponse BasicResponse
+	_, err := s.client.Do(req, &basicResponse)
+	if err != nil {
+		return nil, err
+	}
+	if basicResponse.Status != "success" && basicResponse.Status != "" {
+		return nil, errors.New(basicResponse.Message)
+	}
+
+	return &basicResponse, nil
 }
--- utho/vpc_test.go
+++ utho/vpc_test.go
@@ -7,32 +7,33 @@ import (
 	"reflect"
 	"testing"
 
+	"github.com/go-faker/faker/v4"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestVpcService_Create_happyPath(t *testing.T) {
 	token := "token"
-	payload := CreateVpcParams{
-		Dcslug:  "innoida",
-		Name:    "testq",
-		Planid:  "1008",
-		Network: "10.200.210.1",
-		Size:    "24",
-	}
+	var payload CreateVpcParams
+	_ = faker.FakeData(&payload)
 
 	client, mux, _, teardown := setup(token)
 	defer teardown()
 
+	var fakeResp CreateResponse
+	_ = faker.FakeData(&fakeResp)
+	fakeResp.Status = "success"
+	respBytes, _ := json.Marshal(fakeResp)
+
 	mux.HandleFunc("/vpc/create", func(w http.ResponseWriter, req *http.Request) {
 		testHttpMethod(t, req, http.MethodPost)
 		testHeader(t, req, "Authorization", "Bearer "+token)
-		fmt.Fprint(w, dummyCreateResponseJson)
+		w.Write(respBytes)
 	})
 
 	got, err := client.Vpc().Create(payload)
 
 	var want CreateResponse
-	_ = json.Unmarshal([]byte(dummyCreateResponseJson), &want)
+	_ = json.Unmarshal(respBytes, &want)
 
 	assert.Nil(t, err)
 	assert.Equal(t, want, *got)
@@ -51,20 +52,27 @@ func TestVpcService_Read_happyPath(t *testing.T) {
 	client, mux, _, teardown := setup("token")
 	defer teardown()
 
-	vpcId := "qwsdrf1-1bfa-46ef-8b94-f69f3qwszcf"
-	expectedResponse := dummyReadVpcRes
-	serverResponse := dummyVpcServerRes
+	var fakeVpc Vpc
+	_ = faker.FakeData(&fakeVpc)
+	fakeVpc.Status = "success"
+	serverResp := struct {
+		Vpc []Vpc `json:"vpc"`
+	}{
+		Vpc: []Vpc{fakeVpc},
+	}
+	serverResponse, _ := json.Marshal(serverResp)
+	expectedResponse, _ := json.Marshal(fakeVpc)
 
 	mux.HandleFunc("/vpc", func(w http.ResponseWriter, req *http.Request) {
 		testHttpMethod(t, req, "GET")
 		testHeader(t, req, "Authorization", "Bearer token")
-		fmt.Fprint(w, serverResponse)
+		w.Write(serverResponse)
 	})
 
 	var want Vpc
-	_ = json.Unmarshal([]byte(expectedResponse), &want)
+	_ = json.Unmarshal(expectedResponse, &want)
 
-	got, _ := client.Vpc().Read(vpcId)
+	got, _ := client.Vpc().Read(fakeVpc.ID)
 	if !reflect.DeepEqual(*got, want) {
 		t.Errorf("Response = %v, want %v", *got, want)
 	}
@@ -73,12 +81,12 @@ func TestVpcService_Read_happyPath(t *testing.T) {
 func TestVpcService_Read_invalidServer(t *testing.T) {
 	client, _ := NewClient("token")
 
-	apikey, err := client.Vpc().Read("someId")
+	vpc, err := client.Vpc().Read("someId")
 	if err == nil {
 		t.Errorf("Expected error to be returned")
 	}
-	if apikey != nil {
-		t.Errorf("Was not expecting any apikey to be returned, instead got %v", apikey)
+	if vpc != nil {
+		t.Errorf("Was not expecting any vpc to be returned, instead got %v", vpc)
 	}
 }
 
@@ -86,21 +94,32 @@ func TestVpcService_List_happyPath(t *testing.T) {
 	client, mux, _, teardown := setup("token")
 	defer teardown()
 
-	expectedResponse := dummyListVpcRes
-	serverResponse := dummyVpcServerRes
+	var fakeVpcs []Vpc
+	for i := 0; i < 2; i++ {
+		var v Vpc
+		_ = faker.FakeData(&v)
+		fakeVpcs = append(fakeVpcs, v)
+	}
+	serverResp := struct {
+		Vpc []Vpc `json:"vpc"`
+	}{
+		Vpc: fakeVpcs,
+	}
+	serverResponse, _ := json.Marshal(serverResp)
+	expectedResponse, _ := json.Marshal(fakeVpcs)
 
 	mux.HandleFunc("/vpc", func(w http.ResponseWriter, req *http.Request) {
 		testHttpMethod(t, req, "GET")
 		testHeader(t, req, "Authorization", "Bearer token")
-		fmt.Fprint(w, serverResponse)
+		w.Write(serverResponse)
 	})
 
 	var want []Vpc
-	_ = json.Unmarshal([]byte(expectedResponse), &want)
+	_ = json.Unmarshal(expectedResponse, &want)
 
 	got, _ := client.Vpc().List()
 	if len(got) != len(want) {
-		t.Errorf("Was expecting %d stacks to be returned, instead got %d", len(want), len(got))
+		t.Errorf("Was expecting %d vpcs to be returned, instead got %d", len(want), len(got))
 	}
 
 	if !reflect.DeepEqual(got, want) {
@@ -111,29 +130,37 @@ func TestVpcService_List_happyPath(t *testing.T) {
 func TestVpcService_List_invalidServer(t *testing.T) {
 	client, _ := NewClient("token")
 
-	stacks, err := client.Vpc().List()
+	vpcs, err := client.Vpc().List()
 	if err == nil {
 		t.Errorf("Expected error to be returned")
 	}
-	if stacks != nil {
-		t.Errorf("Was not expecting any stacks to be returned, instead got %v", stacks)
+	if vpcs != nil {
+		t.Errorf("Was not expecting any vpcs to be returned, instead got %v", vpcs)
 	}
 }
 
 func TestVpcService_Delete_happyPath(t *testing.T) {
 	token := "token"
-	vpcId := "qwsdrf1-1bfa-46ef-8b94-f69f3qwszcf"
+	var fakeVpc Vpc
+	_ = faker.FakeData(&fakeVpc)
+	vpcId := fakeVpc.ID
 
 	client, mux, _, teardown := setup(token)
 	defer teardown()
 
+	var fakeResp DeleteResponse
+	_ = faker.FakeData(&fakeResp)
+	fakeResp.Status = "success"
+	fakeResp.Message = "success"
+	respBytes, _ := json.Marshal(fakeResp)
+
 	mux.HandleFunc("/vpc/"+vpcId+"/destroy", func(w http.ResponseWriter, req *http.Request) {
 		testHttpMethod(t, req, "DELETE")
 		testHeader(t, req, "Authorization", "Bearer "+token)
-		fmt.Fprint(w, dummyDeleteResponseJson)
+		w.Write(respBytes)
 	})
 
-	want := DeleteResponse{Status: "success", Message: "success"}
+	want := fakeResp
 
 	got, _ := client.Vpc().Delete(vpcId)
 	if !reflect.DeepEqual(*got, want) {
@@ -153,23 +180,449 @@ func TestVpcService_Delete_invalidServer(t *testing.T) {
 	}
 }
 
-const dummyReadVpcRes = `{
-	"id": "qwsdrf1-1bfa-46ef-8b94-f69f3qwszcf",
-	"total": 254,
-	"available": 248,
-	"network": "10.210.100.0",
-	"name": "test",
-	"size": "24",
-	"dcslug": "innoida",
-	"dclocation": {
-		"dccc": "in",
-		"location": "Delhi (Noida)"
-	},
-	"is_default": "0"
-}`
+func TestVpcService_CreateSubnet_happyPath(t *testing.T) {
+	token := "token"
+	var payload CreateSubnetParams
+	_ = faker.FakeData(&payload)
 
-const dummyVpcServerRes = `{
-    "vpc": [` + dummyReadVpcRes + `]
-}`
+	client, mux, _, teardown := setup(token)
+	defer teardown()
 
-const dummyListVpcRes = `[` + dummyReadVpcRes + `]`
+	var fakeResp CreateResponse
+	_ = faker.FakeData(&fakeResp)
+	fakeResp.Status = "success"
+	respBytes, _ := json.Marshal(fakeResp)
+
+	mux.HandleFunc("/vpc/subnet/create", func(w http.ResponseWriter, req *http.Request) {
+		testHttpMethod(t, req, http.MethodPost)
+		testHeader(t, req, "Authorization", "Bearer "+token)
+		w.Write(respBytes)
+	})
+
+	got, err := client.Vpc().CreateSubnet(payload)
+
+	var want CreateResponse
+	_ = json.Unmarshal(respBytes, &want)
+
+	assert.Nil(t, err)
+	assert.Equal(t, want, *got)
+}
+
+func TestVpcService_CreateSubnet_invalidServer(t *testing.T) {
+	client, _ := NewClient("token")
+
+	_, err := client.Vpc().CreateSubnet(CreateSubnetParams{})
+	if err == nil {
+		t.Errorf("Expected error to be returned")
+	}
+}
+
+func TestVpcService_CreateNatGateway_happyPath(t *testing.T) {
+	token := "token"
+	var payload CreateNatGatewayParams
+	_ = faker.FakeData(&payload)
+
+	client, mux, _, teardown := setup(token)
+	defer teardown()
+
+	var fakeResp CreateResponse
+	_ = faker.FakeData(&fakeResp)
+	fakeResp.Status = "success"
+	respBytes, _ := json.Marshal(fakeResp)
+
+	mux.HandleFunc("/vpc/natgateway", func(w http.ResponseWriter, req *http.Request) {
+		testHttpMethod(t, req, http.MethodPost)
+		testHeader(t, req, "Authorization", "Bearer "+token)
+		w.Write(respBytes)
+	})
+
+	got, err := client.Vpc().CreateNatGateway(payload)
+
+	var want CreateResponse
+	_ = json.Unmarshal(respBytes, &want)
+
+	assert.Nil(t, err)
+	assert.Equal(t, want, *got)
+}
+
+func TestVpcService_CreateNatGateway_invalidServer(t *testing.T) {
+	client, _ := NewClient("token")
+
+	_, err := client.Vpc().CreateNatGateway(CreateNatGatewayParams{})
+	if err == nil {
+		t.Errorf("Expected error to be returned")
+	}
+}
+
+func TestVpcService_AllocateElasticIP_happyPath(t *testing.T) {
+	token := "token"
+	var payload AllocateElasticIPParams
+	_ = faker.FakeData(&payload)
+
+	client, mux, _, teardown := setup(token)
+	defer teardown()
+
+	var fakeResp CreateResponse
+	_ = faker.FakeData(&fakeResp)
+	fakeResp.Status = "success"
+	respBytes, _ := json.Marshal(fakeResp)
+
+	mux.HandleFunc("/elasticip/allocate", func(w http.ResponseWriter, req *http.Request) {
+		testHttpMethod(t, req, http.MethodPost)
+		testHeader(t, req, "Authorization", "Bearer "+token)
+		w.Write(respBytes)
+	})
+
+	got, err := client.Vpc().AllocateElasticIP(payload)
+
+	var want CreateResponse
+	_ = json.Unmarshal(respBytes, &want)
+
+	assert.Nil(t, err)
+	assert.Equal(t, want, *got)
+}
+
+func TestVpcService_AllocateElasticIP_invalidServer(t *testing.T) {
+	client, _ := NewClient("token")
+
+	_, err := client.Vpc().AllocateElasticIP(AllocateElasticIPParams{})
+	if err == nil {
+		t.Errorf("Expected error to be returned")
+	}
+}
+
+func TestVpcService_DeallocateElasticIP_happyPath(t *testing.T) {
+	token := "token"
+	var payload DeallocateElasticIPParams
+	_ = faker.FakeData(&payload)
+	payload.IPAddress = faker.IPv4()
+
+	client, mux, _, teardown := setup(token)
+	defer teardown()
+
+	var fakeResp BasicResponse
+	_ = faker.FakeData(&fakeResp)
+	fakeResp.Status = "success"
+	respBytes, _ := json.Marshal(fakeResp)
+
+	mux.HandleFunc(fmt.Sprintf("/elasticip/%s/deallocate", payload.IPAddress), func(w http.ResponseWriter, req *http.Request) {
+		testHttpMethod(t, req, http.MethodPost)
+		testHeader(t, req, "Authorization", "Bearer "+token)
+		w.Write(respBytes)
+	})
+
+	got, err := client.Vpc().DeallocateElasticIP(payload)
+
+	var want BasicResponse
+	_ = json.Unmarshal(respBytes, &want)
+
+	assert.Nil(t, err)
+	assert.Equal(t, want, *got)
+}
+
+func TestVpcService_DeallocateElasticIP_invalidServer(t *testing.T) {
+	client, _ := NewClient("token")
+
+	_, err := client.Vpc().DeallocateElasticIP(DeallocateElasticIPParams{})
+	if err == nil {
+		t.Errorf("Expected error to be returned")
+	}
+}
+
+func TestVpcService_CreatePeeringConnection_happyPath(t *testing.T) {
+	token := "token"
+	var payload CreatePeeringConnectionParams
+	_ = faker.FakeData(&payload)
+
+	client, mux, _, teardown := setup(token)
+	defer teardown()
+
+	var fakeResp CreateResponse
+	_ = faker.FakeData(&fakeResp)
+	fakeResp.Status = "success"
+	respBytes, _ := json.Marshal(fakeResp)
+
+	mux.HandleFunc("/vpc/peering", func(w http.ResponseWriter, req *http.Request) {
+		testHttpMethod(t, req, http.MethodPost)
+		testHeader(t, req, "Authorization", "Bearer "+token)
+		w.Write(respBytes)
+	})
+
+	got, err := client.Vpc().CreatePeeringConnection(payload)
+
+	var want CreateResponse
+	_ = json.Unmarshal(respBytes, &want)
+
+	assert.Nil(t, err)
+	assert.Equal(t, want, *got)
+}
+
+func TestVpcService_CreatePeeringConnection_invalidServer(t *testing.T) {
+	client, _ := NewClient("token")
+
+	_, err := client.Vpc().CreatePeeringConnection(CreatePeeringConnectionParams{})
+	if err == nil {
+		t.Errorf("Expected error to be returned")
+	}
+}
+
+func TestVpcService_UpdateRoute_happyPath(t *testing.T) {
+	token := "token"
+	var payload UpdateRouteParams
+	_ = faker.FakeData(&payload)
+
+	client, mux, _, teardown := setup(token)
+	defer teardown()
+
+	var fakeResp BasicResponse
+	_ = faker.FakeData(&fakeResp)
+	fakeResp.Status = "success"
+	respBytes, _ := json.Marshal(fakeResp)
+
+	mux.HandleFunc("/vpc/route", func(w http.ResponseWriter, req *http.Request) {
+		testHttpMethod(t, req, http.MethodPost)
+		testHeader(t, req, "Authorization", "Bearer "+token)
+		w.Write(respBytes)
+	})
+
+	got, err := client.Vpc().UpdateRoute(payload)
+
+	var want BasicResponse
+	_ = json.Unmarshal(respBytes, &want)
+
+	assert.Nil(t, err)
+	assert.Equal(t, want, *got)
+}
+
+func TestVpcService_UpdateRoute_invalidServer(t *testing.T) {
+	client, _ := NewClient("token")
+
+	_, err := client.Vpc().UpdateRoute(UpdateRouteParams{})
+	if err == nil {
+		t.Errorf("Expected error to be returned")
+	}
+}
+
+func TestVpcService_CreateInternetGateway_happyPath(t *testing.T) {
+	token := "token"
+	var payload CreateInternetGatewayParams
+	_ = faker.FakeData(&payload)
+
+	client, mux, _, teardown := setup(token)
+	defer teardown()
+
+	var fakeResp CreateResponse
+	_ = faker.FakeData(&fakeResp)
+	fakeResp.Status = "success"
+	respBytes, _ := json.Marshal(fakeResp)
+
+	mux.HandleFunc("/vpc-internetgateway", func(w http.ResponseWriter, req *http.Request) {
+		testHttpMethod(t, req, http.MethodPost)
+		testHeader(t, req, "Authorization", "Bearer "+token)
+		w.Write(respBytes)
+	})
+
+	got, err := client.Vpc().CreateInternetGateway(payload)
+
+	var want CreateResponse
+	_ = json.Unmarshal(respBytes, &want)
+
+	assert.Nil(t, err)
+	assert.Equal(t, want, *got)
+}
+
+func TestVpcService_CreateInternetGateway_invalidServer(t *testing.T) {
+	client, _ := NewClient("token")
+
+	_, err := client.Vpc().CreateInternetGateway(CreateInternetGatewayParams{})
+	if err == nil {
+		t.Errorf("Expected error to be returned")
+	}
+}
+
+func TestVpcService_AttachSubnetToInternetGateway_happyPath(t *testing.T) {
+	token := "token"
+	var payload AttachSubnetToInternetGatewayParams
+	_ = faker.FakeData(&payload)
+	payload.GatewayID = faker.UUIDDigit()
+
+	client, mux, _, teardown := setup(token)
+	defer teardown()
+
+	var fakeResp BasicResponse
+	_ = faker.FakeData(&fakeResp)
+	fakeResp.Status = "success"
+	respBytes, _ := json.Marshal(fakeResp)
+
+	mux.HandleFunc(fmt.Sprintf("/vpc-internetgateway/%s/attach", payload.GatewayID), func(w http.ResponseWriter, req *http.Request) {
+		testHttpMethod(t, req, http.MethodPost)
+		testHeader(t, req, "Authorization", "Bearer "+token)
+		w.Write(respBytes)
+	})
+
+	got, err := client.Vpc().AttachSubnetToInternetGateway(payload)
+
+	var want BasicResponse
+	_ = json.Unmarshal(respBytes, &want)
+
+	assert.Nil(t, err)
+	assert.Equal(t, want, *got)
+}
+
+func TestVpcService_AttachSubnetToInternetGateway_invalidServer(t *testing.T) {
+	client, _ := NewClient("token")
+
+	_, err := client.Vpc().AttachSubnetToInternetGateway(AttachSubnetToInternetGatewayParams{})
+	if err == nil {
+		t.Errorf("Expected error to be returned")
+	}
+}
+
+func TestVpcService_DetachSubnetFromInternetGateway_happyPath(t *testing.T) {
+	token := "token"
+	var payload DetachSubnetFromInternetGatewayParams
+	_ = faker.FakeData(&payload)
+	payload.GatewayID = faker.UUIDDigit()
+
+	client, mux, _, teardown := setup(token)
+	defer teardown()
+
+	var fakeResp BasicResponse
+	_ = faker.FakeData(&fakeResp)
+	fakeResp.Status = "success"
+	respBytes, _ := json.Marshal(fakeResp)
+
+	mux.HandleFunc(fmt.Sprintf("/vpc-internetgateway/%s/dettach", payload.GatewayID), func(w http.ResponseWriter, req *http.Request) {
+		testHttpMethod(t, req, http.MethodPost)
+		testHeader(t, req, "Authorization", "Bearer "+token)
+		w.Write(respBytes)
+	})
+
+	got, err := client.Vpc().DetachSubnetFromInternetGateway(payload)
+
+	var want BasicResponse
+	_ = json.Unmarshal(respBytes, &want)
+
+	assert.Nil(t, err)
+	assert.Equal(t, want, *got)
+}
+
+func TestVpcService_DetachSubnetFromInternetGateway_invalidServer(t *testing.T) {
+	client, _ := NewClient("token")
+
+	_, err := client.Vpc().DetachSubnetFromInternetGateway(DetachSubnetFromInternetGatewayParams{})
+	if err == nil {
+		t.Errorf("Expected error to be returned")
+	}
+}
+
+func TestVpcService_CreateRouteTable_happyPath(t *testing.T) {
+	token := "token"
+	var payload CreateRouteTableParams
+	_ = faker.FakeData(&payload)
+
+	client, mux, _, teardown := setup(token)
+	defer teardown()
+
+	var fakeResp CreateResponse
+	_ = faker.FakeData(&fakeResp)
+	fakeResp.Status = "success"
+	respBytes, _ := json.Marshal(fakeResp)
+
+	mux.HandleFunc("/vpc-route-table", func(w http.ResponseWriter, req *http.Request) {
+		testHttpMethod(t, req, http.MethodPost)
+		testHeader(t, req, "Authorization", "Bearer "+token)
+		w.Write(respBytes)
+	})
+
+	got, err := client.Vpc().CreateRouteTable(payload)
+
+	var want CreateResponse
+	_ = json.Unmarshal(respBytes, &want)
+
+	assert.Nil(t, err)
+	assert.Equal(t, want, *got)
+}
+
+func TestVpcService_CreateRouteTable_invalidServer(t *testing.T) {
+	client, _ := NewClient("token")
+
+	_, err := client.Vpc().CreateRouteTable(CreateRouteTableParams{})
+	if err == nil {
+		t.Errorf("Expected error to be returned")
+	}
+}
+
+func TestVpcService_AssociateSubnetWithRouteTable_happyPath(t *testing.T) {
+	token := "token"
+	var payload AssociateSubnetWithRouteTableParams
+	_ = faker.FakeData(&payload)
+
+	client, mux, _, teardown := setup(token)
+	defer teardown()
+
+	var fakeResp BasicResponse
+	_ = faker.FakeData(&fakeResp)
+	fakeResp.Status = "success"
+	respBytes, _ := json.Marshal(fakeResp)
+
+	mux.HandleFunc("/vpc/subnet/associate", func(w http.ResponseWriter, req *http.Request) {
+		testHttpMethod(t, req, http.MethodPost)
+		testHeader(t, req, "Authorization", "Bearer "+token)
+		w.Write(respBytes)
+	})
+
+	got, err := client.Vpc().AssociateSubnetWithRouteTable(payload)
+
+	var want BasicResponse
+	_ = json.Unmarshal(respBytes, &want)
+
+	assert.Nil(t, err)
+	assert.Equal(t, want, *got)
+}
+
+func TestVpcService_AssociateSubnetWithRouteTable_invalidServer(t *testing.T) {
+	client, _ := NewClient("token")
+
+	_, err := client.Vpc().AssociateSubnetWithRouteTable(AssociateSubnetWithRouteTableParams{})
+	if err == nil {
+		t.Errorf("Expected error to be returned")
+	}
+}
+
+func TestVpcService_DissociateSubnetFromRouteTable_happyPath(t *testing.T) {
+	token := "token"
+	var payload DissociateSubnetFromRouteTableParams
+	_ = faker.FakeData(&payload)
+
+	client, mux, _, teardown := setup(token)
+	defer teardown()
+
+	var fakeResp BasicResponse
+	_ = faker.FakeData(&fakeResp)
+	fakeResp.Status = "success"
+	respBytes, _ := json.Marshal(fakeResp)
+
+	mux.HandleFunc("/vpc/subnet/dissociate", func(w http.ResponseWriter, req *http.Request) {
+		testHttpMethod(t, req, http.MethodPost)
+		testHeader(t, req, "Authorization", "Bearer "+token)
+		w.Write(respBytes)
+	})
+
+	got, err := client.Vpc().DissociateSubnetFromRouteTable(payload)
+
+	var want BasicResponse
+	_ = json.Unmarshal(respBytes, &want)
+
+	assert.Nil(t, err)
+	assert.Equal(t, want, *got)
+}
+
+func TestVpcService_DissociateSubnetFromRouteTable_invalidServer(t *testing.T) {
+	client, _ := NewClient("token")
+
+	_, err := client.Vpc().DissociateSubnetFromRouteTable(DissociateSubnetFromRouteTableParams{})
+	if err == nil {
+		t.Errorf("Expected error to be returned")
+	}
+}
